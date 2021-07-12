ec315b0a3dbda93e29603b271707d78516301610
mychoi <!6648>
2021-07-12 19:50:28 +0900

    @Configuration을 통한 빈등록

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,42 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.impl.MemberServiceImpl;
+import hello.core.member.impl.MemoryMemberRepository;
+import hello.core.order.impl.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        MemoryMemberRepository memberRepository = ac.getBean("memberRepository", MemoryMemberRepository.class);
+
+        System.out.println("memberService => MemberRepository = " + memberRepository1);
+        System.out.println("orderService => MemberRepository = " + memberRepository2);
+        System.out.println("memoryMemberRepository => MemoryMemberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberRepository1).isNotSameAs(memberRepository);
+        Assertions.assertThat(memberRepository2).isNotSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig appConfig = ac.getBean("appConfig", AppConfig.class);
+        System.out.println(appConfig);
+    }
+}
